a03af09e87bfc66aced9509a3bdfc1c9d8ed4421
Brandow Buenos <brandowbuenos@gmail.com>
2020-06-06 18:43:31 -0300

    feat: improvements in Home Page

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,9 +1,17 @@
-
 import React from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
 import { View, ImageBackground, Image, StyleSheet, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  function handleNavigationToPoints() {
+    navigation.navigate('Points');
+  }
+
   return (
     <ImageBackground
       source={require('../../assets/home-background.png')} style={styles.container}
@@ -13,12 +21,19 @@ const Home = () => {
         <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
         <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coletas eficientes.</Text>
       </View>
-
       <View style={styles.footer}>
-
+        <RectButton style={styles.button} onPress={handleNavigationToPoints}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+            </Text>
+        </RectButton>
       </View>
-
-    </ImageBackground>)
+    </ImageBackground>);
 };
 
 const styles = StyleSheet.create({
